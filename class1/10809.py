30467fb1999b2ad73f0ecaf54dbb951ec9625cc7
Arthur <nageonwoo2783@naver.com>
2021-05-21 20:21:47 -0400

    boj 10871

--- class1/10809.py
+++ class1/10809.py
@@ -1,26 +1,18 @@
-# alphabet = "abcdefghijklmnopqrstuvwxyz"
-# word_list = []
-# word_num = []
-#
-# word = str(input())
-#
-#
-# for i in word:
-#     word_list.append(i)
-#
-# for character in alphabet:
-#     for i in range(len(word_list)):
-#         if character == word_list[i]:
-#             word_num.append(i)
-#             break
-#         elif i < len(word_list)-1: continue
-#         else:
-#             word_num.append(-1)
-# for i in word_num:
-#     print(i,end=" ")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+word_list = []
+word_num = []
 
-S=int()
+word = str(input())
+for i in word:
+    word_list.append(i)
 
-alp=[-1]*26
-
-for i in range(len(S)):
+for character in alphabet:
+    for i in range(len(word_list)):
+        if character == word_list[i]:
+            word_num.append(i)
+            break
+        elif i < len(word_list)-1: continue
+        else:
+            word_num.append(-1)
+for i in word_num:
+    print(i,end=" ")
